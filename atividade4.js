d73cbbbadb7b75c50f872a9a71d18c661cca46f4
GabrielPortalBertollo <gabriel_bertollo2002@hotmail.com>
2024-05-17 01:40:13 -0300

    Atividade 4 e 5 feitas

--- atividade4.js
+++ atividade4.js
@@ -2,3 +2,18 @@
 comprimentos e diga se é possível formar um triângulo com essas retas.
 Matematicamente, para três segmentos formarem um triângulo, o comprimento de cada
 lado deve ser menor que a soma dos outros dois.*/
+
+const prompt= require('prompt-sync')();
+let lados= [];
+lados[0]= parseFloat(prompt("Digite o tamanho do primeiro lado: "))
+lados[1]= parseFloat(prompt("Digite o tamanho do segundo lado: "))
+lados[2]= parseFloat(prompt("Digite o tamanho do terceiro lado: "))
+
+for (let i = 0; i < 3; i++) {
+    let soma= lados[0]+lados[1]+lados[2]-lados[i];
+    if(lados[i]>=soma){
+        console.log("Os valores fornecidos não formam um triângulo.")
+        return;
+    }
+}
+console.log("Estes 3 valores formam um triângulo.")
---
+++ atividade5.js
@@ -0,0 +1,31 @@
+/*Crie um jogo de JoKenPo (Pedra-Papel-Tesoura).*/
+const prompt= require('prompt-sync')();
+
+console.log("Vamos jogar!")
+    let jogador;
+    let ia=Math.floor(Math.random() * 3)+1;
+    console.log("Intruções: digite os seguintes comandos para cada ação:\n1= Pedra\n2= Papel\n3= Tesoura\nQue os jogos comecem!!!")
+    do {
+        let jogador=prompt("Sua jogada: ")
+        if(jogador!=1&&jogador!=2&&jogador!=3){console.log("Jogada inválida, fim do jogo.");return}//verificação de jogada válida
+        ia=Math.floor(Math.random() * 3)+1;
+        console.log("A ia jogou: "+ia)
+        ganhador=verificar(jogador,ia)
+        if(ganhador==3){console.log("Empate, vamos novamente.")}
+    } while (ganhador==3);
+    if(ganhador==1){console.log("Parabéns, você ganhou!!!")}
+    else{console.log("Infelizmente você perdeu, mais sorte na próxima.")}
+
+
+
+
+
+
+
+function verificar(j1,j2) {
+    if(j1==j2){return 3}//empate
+    if(j1==1 && j2==2){return 2}//vitoria do jogador 2 por papel
+    if(j1==2 && j2==3){return 2}//vitoria do jogador 2 por tesoura
+    if(j1==3 && j2==1){return 2}//vitoria do jogador 2 por pedra
+    return 1//se não deu empate e o jogador 2 não ganhou em nenhuma opção possivel então o jogador 1 ganhou
+}
